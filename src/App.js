8806abf146d3acdd10f2a271d4d10e3cd934d397
Bharadwaj <kunabharadwaj520@gmail.com>
2022-09-01 21:09:37 +0100

    ToDoList App version-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: 'Kuna',
+      todoItems: [
+        {action: 'Buy Milk', done: false},
+        {action: 'Buy Veggies', done: false},
+        {action: 'Buy Drinks', done: false},
+      ],
+      newToDo: '',
+    }
+  }
+  updateValue = (event) => {
+    this.setState({newToDo: event.target.value})
+  }
+  newToDo = (event) => {
+    this.setState({
+      todoItems:[
+        ...this.state.todoItems,
+        {action: this.state.newToDo, done:false}
+      ]
+    })
+  }
+  toogleDone = (todo) => 
+  this.setState({
+    todoItems: this.state.todoItems.map((item)=>
+    item.action === todo.action ? { ...item, done : !item.done} : item
+    ),
+  })
+  todoRows = () => 
+    this.state.todoItems.map((item)=>(
+      <tr key = {item.action}>
+        <td>{item.action}</td>
+        <td>
+          <input type='checkbox' checked = {item.done} onChange = {()=> this.toogleDone(item)}></input>
+        </td>
+      </tr>
+    ))
+  render = () => (
+    <div className = 'container'>
+      <div className = 'row'>
+        <div className='col-12'>
+          <h2 className='bg-primary text-white text-center p2'>
+            {this.state.userName} To Do List
+          </h2>
+        </div>
+        <div className='col-12'>
+          <input className='from-control' value = {this.state.newToDo} onChange={this.updateValue}></input>
+          <button className='btn btn-primary' onClick = {this.newToDo}>Add</button>
+        </div>
+        {/* <button className='btn btn-primary' onClick = {this.newToDo}>Add</button> */}
+        <div className='col-12'>
+          <table className='table'>
+            <thead>
+              <tr>
+                <th>
+                  Task
+                </th>
+                <th>Complete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.todoRows()}
+            </tbody>
+
+          </table>
+
+        </div>
+      </div>
+  
     </div>
-  );
+  )
 }
 
-export default App;
